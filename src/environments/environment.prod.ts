c89a2371224c6fc108a6f5b76f0b98fe540cccca
Vanya Dimitrova <vanyad@gmail.com>
2020-12-20 12:11:46 +0200

    Prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDrYXgekCkHC8_Ym-a6hlfumGgthkZkkVs',
+    authDomain: 'v-design-5.firebaseapp.com',
+    databaseURL: 'https://v-design-5.firebaseio.com',
+    projectId: 'v-design-5',
+    storageBucket: 'v-design-5.appspot.com',
+    messagingSenderId: '790520967482',
+    appId: '1:790520967482:web:e0a0970e55bb1b1412e003',
+    measurementId: 'G-MBKKVLX1QS'
+  }
 };
