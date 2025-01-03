f9eeba844808d247b7e1cfa12abd1855d7d9ec13
VD <vanyad@gmail.com>
2025-01-03 20:00:50 +0200

    Update project

---
+++ src/app/moodle/moodle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moodle',
+  imports: [],
+  templateUrl: './moodle.component.html',
+  styleUrl: './moodle.component.scss'
+})
+export class MoodleComponent {
+
+}
